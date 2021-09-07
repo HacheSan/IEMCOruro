d80e301dc953d47455e3da8d3d3695c42e22ea9e
unknown <a.colq.c@hotmail.com>
2021-09-07 17:58:44 -0400

    Modificacion de rutas

--- routes/eco.php
+++ routes/eco.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/eco', function(){
+Route::get('/', function(){
     return "Hola eco";
 })->name('eco.home');
--- routes/secre.php
+++ routes/secre.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/secre', function(){
+Route::get('/', function(){
     return "Hola secre";
 })->name('secre.home');
