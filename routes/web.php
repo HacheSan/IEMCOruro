bd87cb9ba739f5eead7c1ba247bc72460e7510c6
unknown <a.colq.c@hotmail.com>
2021-09-27 17:37:00 -0400

    Page Welcome Show Activities

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Activity;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $activities = Activity::orderBy('id', 'desc')->get();
+    return view('welcome', compact('activities'));
 });
 
 Auth::routes();
